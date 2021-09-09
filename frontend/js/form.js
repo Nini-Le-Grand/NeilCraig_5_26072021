28920e69c8b85a586475d2134c81e4548b93ac0a
Neil <neilcraig75017@gmail.com>
2021-09-09 17:59:32 +0200

    Integration oubli validation formulaire

--- frontend/js/form.js
+++ frontend/js/form.js
@@ -50,50 +50,88 @@ function setRequest() {
 }
 
 function listenToSubmit() {
-    $("#formulaire").addEventListener("submit", function (e) {
+    $("#submit-btn").addEventListener("click", function (e) {
         e.preventDefault();
-        let request = setRequest();
-        sendRequest(request);
-        //verifyFields();
-        //verifyAllFieldsCompleted()
-        //if(!!verifyFields() && !!verifyAllFieldsCompleted()) {
-        //    let request = setRequest();
-        //    sendRequest(request);
-        //} else {
-        //    return;
-        //}
+        if(verifyAllFieldsCompleted()) {
+            verifyAllRegex()
+        }
     })
 }
 
 function verifyAllFieldsCompleted() {
-    if(!!verifyField(nom, /^[a-zA-ZÀ-ÿ]/) && !!verifyField(prenom, /^[a-zA-ZÀ-ÿ]/) && !!verifyField(email, /\S+@\S+\.\S+/) && !!verifyField(/\S+@\S+\.\S+/) && !!verifyField(address, /^[a-zA-ZÀ-ÿ]/) && !!verifyField(city, /^[a-zA-ZÀ-ÿ]/)) {
+    verifyFieldCompleted('nom')
+    verifyFieldCompleted('prenom')
+    verifyFieldCompleted('address')
+    verifyFieldCompleted('city')
+    verifyFieldCompleted('email')
+    verifyFieldCompleted('confirmation-email')
+
+    if(verifyFieldCompleted('nom') && verifyFieldCompleted('prenom') && verifyFieldCompleted('address') && verifyFieldCompleted("email") && verifyFieldCompleted('confirmation-email') && verifyFieldCompleted('city')) {
         return true;
     } else {
-        console.log("erreur");
-        return;
+        document.getElementById('formAlert').innerHTML = "Certains champs ne sont pas renseignés"
+        return false;
+    }
+    
+}
+
+function verifyFieldCompleted(id) {
+    if(document.getElementById(id).value != '') {
+        document.getElementById(`${id}ValidationSaisie`).innerHTML = ''
+        document.getElementById(id).classList.remove('false-border')
+        document.getElementById(id).classList.add('true-border')
+        return true
+    }
+    else {
+        document.getElementById(`${id}ValidationSaisie`).innerHTML = "Ce champ est obligatoire";
+        document.getElementById(`${id}ValidationSaisie`).classList.add('alertWarning')
+        document.getElementById(id).classList.remove('true-border')
+        document.getElementById(id).classList.add('false-border')
+        return false
     }
 }
 
-function verifyFields() {
-    if(!!verifyRegex()) {
-        return true;
-    } else {
-        return false;
+function verifyAllRegex() {
+    let regexName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/
+    let regexAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/
+    let regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/
+    verifyRegex(regexName, 'nom')
+    verifyRegex(regexName, 'prenom')
+    verifyRegex(regexAddress, 'address')
+    verifyRegex(regexName, 'city')
+    verifyRegex(regexEmail, 'email')
+    verifyRegex(regexEmail, 'confirmation-email')
+
+    if(verifyRegex(regexName, 'nom') && verifyRegex(regexName, 'prenom') && verifyRegex(regexAddress, 'address') && verifyRegex(regexName, 'city') && verifyRegex(regexEmail, 'email') && verifyRegex(regexEmail, 'confirmation-email')) {
+        verifyMatchingEmail()
     }
 }
 
-function verifyRegex(id, regex) {
-    if($(`"#${id}"`).value !== '') {
-        if (regex.test($(`"#${id}"`).value)) {
-            display(`"#${id}ValidationSaisie"`, "Le champ est correctement renseigné")
-            return true;
-        } else {
-            display(`"#${id}ValidationSaisie"`, "La saisie est invalide")
-            return false;
-        }
-        
-    } else if ($(id).value == '') {
-        display(`"#${id}ValidationSaisie"`, "Veuillez renseigner ce champ")
-        return false;
+function verifyRegex(regex, id) {
+    if(regex.test(document.getElementById(id).value)) {
+        document.getElementById(`${id}ValidationSaisie`).innerHTML = '';
+        document.getElementById(id).classList.remove('false-border')
+        document.getElementById(id).classList.add('true-border')
+    } else {
+        document.getElementById(`${id}ValidationSaisie`).innerHTML = "Ce champ n'est pas correctement renseigné";
+        document.getElementById(`${id}ValidationSaisie`).classList.add('alertWarning')
+        document.getElementById(id).classList.remove('true-border')
+        document.getElementById(id).classList.add('false-border')
+    }
+    return regex.test(document.getElementById(id).value)
+}
+
+function verifyMatchingEmail () {
+    if (document.getElementById('email').value == document.getElementById('confirmation-email').value) {
+        let request = setRequest();
+        sendRequest(request);
+    } else {
+        document.getElementById(`emailValidationSaisie`).innerHTML = "les addresses renseignées ne sont pas identiques";
+        document.getElementById(`emailValidationSaisie`).classList.add('alertWarning')
+        document.getElementById(`confirmation-emailValidationSaisie`).classList.add('alertWarning')
+        document.getElementById('email').classList.remove('true-border')
+        document.getElementById('confirmation-email').classList.remove('true-border')
+        document.getElementById('email').classList.add('false-border');
+        document.getElementById('confirmation-email').classList.add('false-border')
     }
 }
